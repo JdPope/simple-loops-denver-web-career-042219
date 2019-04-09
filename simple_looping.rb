c27bdeee942452e07dff015d30cd83290e1bdc88
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-09 20:24:28 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -38,7 +38,7 @@ end
 
 def for_iterator(number_of_times)
 phrase = "Welcome to Flatiron School's Web Development Course!"
-for number_of_times in 1..7 
+for number_of_times in 1..7 do
 puts phrase
 end
 
